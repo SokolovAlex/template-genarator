cec62d7c904c32942ddc12076cd9d2ee1c4337d6
sokolov alex <alex.sklv85@gmail.com>
2019-07-01 23:59:54 +0300

    add template api

--- src/client/api/templates.ts
+++ src/client/api/templates.ts
@@ -2,10 +2,17 @@ import axios from 'axios';
 import { Template } from './../../db/entity/template';
 
 const apiUrl = '/api/templates';
+const conflictUrl = `${apiUrl}/conflicts`;
 
 export const getTemplates = (): Promise<Template[]> => {
-  return axios.get(apiUrl).then((data) => {
-    console.log(data);
-    return [new Template()];
+  return axios.get(apiUrl).then((response) => {
+    const template: Template[] = response.data;
+    return template;
   });
+};
+
+export const checkTemplates = (templates: Template[]): Promise<Template[]> => {
+  return axios.get(conflictUrl, {
+    params: templates.map(template => template.key)
+  }).then((response) => response.data);
 };
--- src/client/containers/Creation/Creation.tsx
+++ src/client/containers/Creation/Creation.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { getTemplates } from './../../api/templates';
+import { Template } from '../../../db/entity/template';
 
 const Creation: React.FC = () => {
+  const [templates, setTemplate ] = React.useState<Template[]>([]);
+  
   React.useEffect(() => {
-    console.info('!!');
-    getTemplates();
+    getTemplates().then((templates) => {
+      setTemplate(templates);
+    });
   }, []);
 
   return (
-    <div>!!!!</div>
+    <div>{
+      templates.map((template) => <div key={template.key}>{template.name}</div>)
+    }</div>
   );
 };
 
